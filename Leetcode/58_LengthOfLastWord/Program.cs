d74a53efdbbe361f6967fb344d2e1a95719df3a2
MAC <MAC@MacBook-Pro.local>
2025-08-24 20:15:46 +0300

    feat: 58 task

---
+++ Leetcode/58_LengthOfLastWord/Program.cs
@@ -0,0 +1,9 @@
+﻿var str = Console.ReadLine();
+
+Console.WriteLine(LengthOfLastWord(str));
+
+int LengthOfLastWord(string s) {
+    string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    return words[words.Length - 1].Length;
+}
