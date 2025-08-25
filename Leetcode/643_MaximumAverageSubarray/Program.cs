0d86e2253e2cd22c3bb3fff61db3b86f5bf27c5a
MAC <MAC@MacBook-Pro.local>
2025-08-25 14:29:09 +0300

    feat: 643 task

---
+++ Leetcode/643_MaximumAverageSubarray/Program.cs
@@ -0,0 +1,27 @@
+﻿var str = Console.ReadLine();
+var k = Convert.ToInt32(Console.ReadLine());
+
+int[] nums = str.Split(' ').Select(int.Parse).ToArray();
+
+Console.WriteLine(FindMaxAverage(nums, k));
+
+double FindMaxAverage(int[] nums, int k)
+{
+     double windowSum = 0;
+
+     for (int i = 0; i < k; i++)
+     {
+          windowSum += nums[i];
+     }
+
+     double avgResult = (double)windowSum / k;
+
+     for (int i = k; i < nums.Length; i++)
+     {
+          windowSum += nums[i] - nums[i - k];
+
+          avgResult = Math.Max(avgResult, windowSum / k);
+     }
+     
+     return avgResult;
+}
