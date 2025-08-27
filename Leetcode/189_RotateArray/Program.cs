138de98281ba2b46a44cb670ecc8b1c7aeeef257
MAC <MAC@MacBook-Pro.local>
2025-08-27 18:30:40 +0300

    feat: 189

---
+++ Leetcode/189_RotateArray/Program.cs
@@ -0,0 +1,23 @@
+﻿
+var nums = new[] { 1, 2, 3, 4, 5, 6, 7 };
+var k = 3;
+Rotate(nums, k);
+
+void Rotate(int[] nums, int k) {
+
+     while (k > 0)
+     {
+          int tempValue = nums[0];
+     
+          nums[0] = nums[nums.Length - 1];
+          
+          for (int i = 1; i < nums.Length; i++)
+          {
+              int temp2 = nums[i];
+              nums[i] = tempValue;
+              tempValue = temp2;
+          }
+     
+          k--;
+     }
+}
