2f15a9f80a2827100298410cd178aebe0e50dee9
MAC <MAC@MacBook-Pro.local>
2025-08-29 12:06:08 +0300

    feat: 268 task

---
+++ Leetcode/268_MissingNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿var nums = new[] { 3, 0, 1 };
+
+Console.WriteLine(MissingNumber(nums));
+
+int MissingNumber(int[] nums)
+{
+        int resultSum = 0;
+        int realSum = 0;
+
+        for (int i = 0; i < nums.Length + 1; i++)
+        {
+                if (i < nums.Length)
+                {
+                        realSum += nums[i];
+                }
+                
+                resultSum += i;
+        }
+
+        return (realSum - resultSum) * -1;
+}
