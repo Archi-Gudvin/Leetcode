162b74f1aee4f28d0172cb48a9b8ea660d791b19
MAC <MAC@MacBook-Pro.local>
2025-09-01 13:33:12 +0300

    feat: 350 task

---
+++ Leetcode/350_IntersectionOfTwoArraysII/Program.cs
@@ -0,0 +1,14 @@
+﻿var nums1 = new[] { 1,2,2,1 };
+var nums2 = new[] { 2,2};
+
+var result = Intersect(nums1, nums2);
+
+foreach (var item in result)
+{
+    Console.Write($"{item} ");
+}
+
+int[] Intersect(int[] nums1, int[] nums2)
+{
+    return nums1.Intersect(nums2).ToArray();
+}
