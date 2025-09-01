614397846e5c7f0ceb07cf3bbb372474482203d9
MAC <MAC@MacBook-Pro.local>
2025-09-01 16:44:18 +0300

    feat: 69 task

---
+++ Leetcode/69_SqrtX/Program.cs
@@ -0,0 +1,39 @@
+﻿
+int x = 67;
+
+Console.WriteLine(MySqrt(x));
+
+int MySqrt(int x)
+{
+    if (x < 2) return x;
+    
+    int left = 0;
+    double right = x;
+
+    var result = BinarySearch(left, right);
+
+    return (int)result;
+    
+    double BinarySearch(double left, double right)
+    {
+        double middleValue = (left + right) / 2;
+
+        double sqrt = middleValue * middleValue < x ? middleValue * middleValue : Math.Round(middleValue * middleValue, 3);
+        
+        if (sqrt == x)
+        {
+            return Math.Floor(middleValue);
+        }
+        else
+        {
+            if (sqrt > x)
+            {
+                return BinarySearch(left, middleValue);
+            }
+            else
+            {
+                return BinarySearch(middleValue, right);
+            }
+        }
+    }
+}
