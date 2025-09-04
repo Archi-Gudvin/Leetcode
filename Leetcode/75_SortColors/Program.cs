39a9ec32d3b0e4b72bc55a674d2c9ad51e210eec
MAC <MAC@MacBook-Pro.local>
2025-09-04 16:22:26 +0300

    feat: 75 task

---
+++ Leetcode/75_SortColors/Program.cs
@@ -0,0 +1,67 @@
+﻿int[] nums = new[] { 2,0,2,1,1,0 };
+
+SortColors(nums);
+
+void SortColors(int[] nums)
+{
+    nums = QuickSort(nums);
+}
+
+int[] QuickSort(int[] nums)
+{
+    Sort(0, nums.Length - 1);
+
+    return nums;
+    
+    void Sort(int low, int high)
+    {
+        if (high <= low)
+            return;
+
+        var j = Partition(low, high);
+
+        Sort(low, j - 1);
+        Sort(j + 1, high);
+    }
+
+    int Partition(int low, int high)
+    {
+        int i = low;
+        int j = high + 1;
+
+        var pivot = nums[low];
+
+        while (true)
+        {
+            while (nums[++i] < pivot)
+            {
+                if (i == high)
+                    break;
+            }
+
+            while (pivot < nums[--j])
+            {
+                if (j == low)
+                    break;
+            }
+
+            if (i >= j)
+                break;
+            
+            Swap(i, j);
+        }
+        
+        Swap(low, j);
+
+        return j;
+    }
+
+    void Swap(int i, int j)
+    {
+        if (i == j)
+            return;
+        var temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
