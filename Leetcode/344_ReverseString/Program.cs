9897210b15682e1d0fdaa87489a0ab4ff574dbd1
MAC <MAC@MacBook-Pro.local>
2025-09-09 18:08:45 +0300

    feat: 344 task

---
+++ Leetcode/344_ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿char[] s = ['h', 'e', 'l', 'l', 'o'];
+ReverseString(s);
+
+void ReverseString(char[] s)
+{
+    int k = s.Length - 1;
+    int i = 0;
+    
+    while(i < k)
+    {
+        if (s[i] != s[k])
+        {
+            (s[i], s[k]) = (s[k], s[i]);
+        }
+
+        i++;
+        k--;
+    }
+
+    foreach (var item in s)
+    {
+        Console.WriteLine(item);
+    }
+}
