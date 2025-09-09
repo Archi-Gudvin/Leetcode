95b203bfac4551879d3fa6e7bf7bb6e2a3d73643
MAC <MAC@MacBook-Pro.local>
2025-09-09 18:48:36 +0300

    feat: 242 task

---
+++ Leetcode/242_ValidAnagram/Program.cs
@@ -0,0 +1,20 @@
+﻿string s = "anagram", t = "nagaram";
+Console.WriteLine(IsAnagram(s, t));
+
+
+bool IsAnagram(string s, string t)
+{
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        var sArr = s.ToCharArray();
+        
+        Array.Sort(sArr);
+        
+        var tArr = t.ToCharArray();
+        
+        Array.Sort(tArr);
+        return sArr.SequenceEqual(tArr);
+}
