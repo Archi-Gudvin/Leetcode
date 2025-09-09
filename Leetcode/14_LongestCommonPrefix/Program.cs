d50eb0456d2952a9df97a22945e3c1a6ed4e599d
MAC <MAC@MacBook-Pro.local>
2025-09-09 17:33:57 +0300

    feat: 14 task

---
+++ Leetcode/14_LongestCommonPrefix/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] strs = ["flower", "flow", "flight", "florew", "flphfg"];
+
+Console.WriteLine(LongestCommonPrefix(strs));
+
+
+string LongestCommonPrefix(string[] strs)
+{
+        if (strs.Length == 0)
+        {
+                return "";
+        }
+        
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                        prefix = prefix.Substring(0, prefix.Length - 1);
+
+                        if (prefix == "")
+                        {
+                                return "";
+                        }
+                }
+        }
+        
+        return prefix;
+}
