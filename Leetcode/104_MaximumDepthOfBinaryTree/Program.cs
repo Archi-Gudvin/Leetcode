cc1d5cb3eb923baf50dd9933b771b5478e4a24df
MAC <MAC@MacBook-Pro.local>
2025-09-10 17:45:04 +0300

    feat: 104 task

---
+++ Leetcode/104_MaximumDepthOfBinaryTree/Program.cs
@@ -0,0 +1,34 @@
+﻿TreeNode root =
+    new TreeNode(3,
+        new TreeNode(9),
+        new TreeNode(20,
+            new TreeNode(15),
+            new TreeNode(7)
+        )
+    );
+
+
+Console.WriteLine(MaxDepth(root));
+
+int MaxDepth(TreeNode root)
+{
+    if (root is null)
+        return 0;
+
+    int leftMaxDepth = MaxDepth(root.left);
+    int rightMaxDepth = MaxDepth(root.right);
+    
+    return Math.Max(leftMaxDepth, rightMaxDepth) + 1;
+}
+
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
