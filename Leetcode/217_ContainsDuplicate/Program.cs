6becdaef631f36d5f840f0c5f5b8592d29f8ab58
aleksandra_masalskaia <masalskyas@gmail.com>
2025-09-15 17:44:14 +0300

    feat: 217 task

---
+++ Leetcode/217_ContainsDuplicate/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] nums = [1, 2, 3];
+
+Console.WriteLine(ContainsDuplicate(nums));
+
+
+bool ContainsDuplicate(int[] nums)
+{
+    HashSet<int> result = new HashSet<int>();
+
+    result.Add(nums[0]);
+
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (result.Contains(nums[i]))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
