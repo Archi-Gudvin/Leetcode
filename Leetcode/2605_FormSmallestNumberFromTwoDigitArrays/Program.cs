5054f130e438d8b0511fcf618c4c4076dc8bad2b
aleksandra_masalskaia <masalskyas@gmail.com>
2025-09-17 19:14:43 +0300

    feat: 2605 task

---
+++ Leetcode/2605_FormSmallestNumberFromTwoDigitArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] nums1 = [4, 1, 3], nums2 = [1, 5, 7];
+
+Console.WriteLine(MinNumber(nums1, nums2));
+
+
+int MinNumber(int[] nums1, int[] nums2)
+{
+    var arrIntersect = nums1.Intersect(nums2);
+
+    string result = "";
+
+    if (!arrIntersect.Any())
+    {
+        int firstMinValue = nums1.Min();
+
+        int secondMinValue = nums2.Min();
+
+        result = $"{firstMinValue}{secondMinValue}";
+    }
+    else
+    {
+        return arrIntersect.Min();
+    }
+
+    return Convert.ToInt32(result);
+}
